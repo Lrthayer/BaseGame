f6f3960b4f926c0756d342ad7caea8415948d659
NobleAbe <linkthayer@sbcglobal.net>
2017-01-06 15:56:08 -0500

    init

---
+++ PlayerForces.js
@@ -0,0 +1,10 @@
+//Player's forces
+
+function PlayerForces()
+{
+	tPlayerForces = new Object();
+	tPlayerForces.number = 100;
+	tPlayerForces.usuable = 100;
+	tPlayerForces.reading = 0;
+	return tPlayerForces;
+}//end PlayerForces
